b6e735255c48158af05c3e57bb19e4cf663d538a
Amirhossein Tayyar <AmirHossein.Tayyar@outlook.com>
2024-01-30 17:45:29 +0330

    feat : SoftwarePart Authorization Add Structure

---
+++ Utilities/SoftwareParts/Authorization/Zamin.Utilities.SoftwareParts.Authorization/Options/SoftwarePartAuthorizationOption.cs
@@ -0,0 +1,20 @@
+﻿namespace Zamin.Utilities.SoftwareParts.Authorization.Options;
+
+public class SoftwarePartAuthorizationOption
+{
+    public string ApplicationName { get; set; } = string.Empty;
+    public string ModuleName { get; set; } = string.Empty;
+    public string ServiceName { get; set; } = string.Empty;
+    public LoadAccessListOption FetchAccessList { get; set; } = new();
+    public AuthenticationOption Authentication { get; set; } = new();
+}
+
+public class AuthenticationOption
+{
+
+}
+
+public class LoadAccessListOption
+{
+
+}
