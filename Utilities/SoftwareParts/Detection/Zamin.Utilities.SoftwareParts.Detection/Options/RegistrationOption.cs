825c5a330a3db81836813bc7d53cc9cb93e2c4a9
Amirhossein Tayyar <AmirHossein.Tayyar@outlook.com>
2024-01-30 17:07:49 +0330

    .

--- Utilities/SoftwareParts/Detection/Zamin.Utilities.SoftwareParts.Detection/Options/RegistrationOption.cs
+++ Utilities/SoftwareParts/Detection/Zamin.Utilities.SoftwareParts.Detection/Options/RegistrationOption.cs
@@ -2,7 +2,6 @@
 
 public class RegistrationOption
 {
-    public bool Enabled { get; set; } = true;
     public bool IgnoreSSL { get; set; } = false;
     public string BaseAddress { get; set; } = string.Empty;
     public string ServicePath { get; set; } = string.Empty;
